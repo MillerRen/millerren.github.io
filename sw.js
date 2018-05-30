d5fdccc4223dec4c5c2ac3f8f4c615fbd6f40fac
MillerRen <ren12345@126.com>
2018-05-31 07:25:21 +0800

    cache

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
-self.addEventListener('install', function () {
+self.addEventListener('install', function (e) {
   console.log('install')
+  e.waitUntil(caches.open('installation')
+    .then(function (cache) {
+      return cache.addAll([
+        './'
+      ])
+    }))
+})
+
+self.addEventListener('fetch', function (e) {
+  var fetched = fetch(e.request)
+  var cached = caches.match(e.request)
+
+  e.respondWith(fetched.catch(function () {
+    return cached
+  }))
 })
