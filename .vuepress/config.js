2fd37fc4cb610f939095f04eefbd3639383b97c0
MillerRen <ren12345@126.com>
2020-03-10 14:39:23 +0800

    导航

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,8 +4,7 @@ module.exports = {
     description: '海阔任鱼跃，天空任羽飞',
     themeConfig: {
         nav:[
-          { text: '编程', link: '/code/' },
-          { text: '手工', link: '/diy/' },
+          { text: '技术', link: '/tech/' },
           { text: '生活', link: '/life/' },
           { text: 'Github', link: 'https://github.com/MillerRen' }       
         ]
